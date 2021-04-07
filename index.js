31be60e34e9e0ca28f0c6b38bf4c8394ea7952dc
gacarrasquilla <gennys.carrasquilla@platypus-agency.com>
2021-04-07 16:18:11 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+require("./mongodb")
+const express = require("express")
+const charactersRouter = require("./controllers/characters")
+const seriesRouter = require("./controllers/series")
+const handleErrors = require("./middleware/handleErrors")
+const unknownEndpoint = require("./middleware/notFound")
+
+const app = express()
+
+app.use(express.json())
+const PORT = process.env.PORT || 3003
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.use("/api/characters", charactersRouter)
+app.use("/api/series", seriesRouter)
+
+app.use(handleErrors)
+app.use(unknownEndpoint)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
